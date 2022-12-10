ffb0ce4cb26c558225ce04b794d73ab6bc5c65fd
Inna Kirianova <innr@ya.ru>
2022-12-11 00:39:42 +0300

    added code for task42

---
+++ Task42/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3  -> 11
+// 2  -> 10
+
+void BinaryNum(int number)
+{
+    if (number == 0)
+    {
+        return;
+    }
+    BinaryNum(number/2);
+    Console.Write(number%2);
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+BinaryNum(number);
+
+
