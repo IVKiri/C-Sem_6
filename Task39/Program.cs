5974c7b566a4780467fbf7d3e7f6934499e25b61
Inna Kirianova <innr@ya.ru>
2022-12-10 23:54:51 +0300

    added code for task39

---
+++ Task39/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+int[] FillArray(int size)
+{
+    int[] arr = new int[size];
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(1,20);
+    }
+    return arr;
+}
+Console.Write("Введите длину массива: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArray(num);
+Console.WriteLine(string.Join(", ", array));
+
+void ReverseArray(int[] array)
+{
+    for (int i = 0; i < array.Length/2; i++)
+    {
+        int temp = array[i];
+        array[i] =  array[array.Length -i -1];
+        array[array.Length -i -1] = temp;   
+    }
+}
+
+ReverseArray(array);
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+
+
+
